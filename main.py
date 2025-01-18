619227802ac6ed481178535334148e10ce49190f
pchebolu <pchebolu@deloitte.com.au>
2025-01-18 18:14:34 +1100

    adding ship to code

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import pygame
 from constants import * 
+from player import Player
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 
 def main():
    print("Starting asteroids!")
    print("Screen width:", SCREEN_WIDTH)
    print("Screen height:", SCREEN_HEIGHT)
+   p = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+   t = pygame.time.Clock()
+   dt = 0
+
    while True:
+      # p.draw(screen)
       for event in pygame.event.get():
          if event.type == pygame.QUIT:
             return
     
-   screen.fill((0, 0, 0))
-   pygame.display.flip()
+      screen.fill((0, 0, 0))
+      p.update(dt)
+      p.draw(screen)
+      pygame.display.flip()
+      dt = t.tick(60) /1000.0
+   
 
 
 
